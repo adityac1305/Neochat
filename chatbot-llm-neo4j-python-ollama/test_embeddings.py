6e4fb35e2d848caaae8803a3280e55b472ea7bea
Aditya Chaudhari <adityac1305@gmail.com>
2025-09-27 09:34:18 -0400

    neo4j-retriever-tool-ollama

--- chatbot-llm-neo4j-python-ollama/test_embeddings.py
+++ chatbot-llm-neo4j-python-ollama/test_embeddings.py
@@ -1,4 +1,11 @@
+import streamlit as st
 import ollama
-model = "koill/sentence-transformers:all-minilm-l12-v2"
+model = st.secrets["OLLAMA_EMBEDDING_MODEL"]
 print("Models local:", ollama.list())
+
+result = ollama.embeddings(model, ["this is a test"])
+embedding = result["embedding"]
+dimensions = len(embedding)
+print(f"Embedding dimensions: {dimensions}")
+
 print("Embedding test:", ollama.embeddings(model, "this is a test"))
